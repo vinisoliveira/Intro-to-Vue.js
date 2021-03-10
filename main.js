41ae7ef2e82f08a7f5cf9f6458133edf15163628
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2021-03-10 16:13:42 -0400

    Details Added

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg'
+        image: './assets/vmSocks-green-onWhite.jpg',
+        inventory: 15,
+        details: ["80% Cotton",
+                "20% Polyester",
+                "Gender Neutral",
+                "One Size"],
+        variants: [
+            {
+                variantID: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
     }
 })
